004773566455bcc3287773465645e5578d4ebaa9
QingChang166 <qchang@ncsu.edu>
2022-11-28 09:12:23 -0500

    lecture12

--- courses/ps2010/week12/linear_regression_example.R
+++ courses/ps2010/week12/linear_regression_example.R
@@ -1,6 +1,6 @@
 
 
-# we want to know inequality = beta1 + beta2 trade + beta3 fdi + beta4 democracy
+# we want to know Inequality_i = \beta_0 + \beta_1 Democracy + \beta_2 Trade + \beta_3 FDI + \epsilon_i
 
 # run regression using lm()
 
@@ -12,16 +12,16 @@ summary(m1)
 
 ################################################
 #### Now, we begin to calculate OLS manually ####
-################################################ 
+################################################
 
-# 
+#
 
-# First, OLS estimates 
+# First, OLS estimates
 
 
 X <- as.matrix(cbind(1, example_data[ , 2:4])) # Don't forget 1, the intercept column!!!!
 
-colnames(X)[1] <- "Intercept" 
+colnames(X)[1] <- "Intercept"
 
 Y <- as.matrix(example_data$inequality)
 
@@ -51,14 +51,14 @@ k = ncol(X)
 VCV = 1/(n-k) * as.numeric(t(res)%*%res) * solve(t(X)%*%X)
 
 ## Standard errors of the estimated coefficients
-SE = sqrt(diag(VCV))  
+SE = sqrt(diag(VCV))
 
 SE
 
 
-## t value for OLS estimate 
+## t value for OLS estimate
 
-t_value <- round(beta_hat/SE, digits = 5) 
+t_value <- round(beta_hat/SE, digits = 5)
 
 t_value
 
@@ -68,7 +68,7 @@ p_value <- 2*pt( abs(t_value), df = n-k, lower.tail = F)
 
 p_value
 
-## Construct confidence interval 
+## Construct confidence interval
 
 confint(m1)
 
@@ -80,13 +80,15 @@ upper_CI <- beta_hat + qt(0.025, df = n-k, lower.tail = F )*SE
 
 confidence_interval <- cbind(lower_CI, upper_CI)
 
+confidence_interval
 ## Residual standard error
 
 res_V <-  1/(n-k) * as.numeric(t(res)%*%res)
 
 res_SE <- sqrt(res_V)
 
-## R square 
+res_SE
+## R square
 
 total_variance <- sum((Y-mean(Y))^2)
 
@@ -102,14 +104,18 @@ R_square
 
 R_square_adjust <-  1 - (1 - R_square)*(n-1)/(n-k)
 
-
+R_square_adjust
 ## F statistic
 
 explained_variance <-  total_variance - unexplained_variance
 
 F_statistic <- (explained_variance/(k-1))/(unexplained_variance/(n-k))
 
+F_statistic
+
 F_p_value <- pf(F_statistic, k-1, n-k, lower.tail = F)
 
-# Notice!!!!: statistical software uses "p-value: < 2.2e-16" automatically 
+F_p_value
+
+# Notice!!!!: statistical software uses "p-value: < 2.2e-16" automatically
 # for very small numbers. That's why our F_p_value differ from what R report.
